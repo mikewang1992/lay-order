f24874db7ca5a96e23f03bb0a76e8842bb97839a
sun8chi <sun8chi@gmail.com>
2019-12-16 17:15:04 +0800

    login 位置調整、改為 sweet alert、新增本頁樣式

--- src/components/Customer/Member/Login.vue
+++ src/components/Customer/Member/Login.vue
@@ -1,93 +1,47 @@
 <template>
-  <div class="page login">
-    <footer>
-      <ul>
-        <!-- <li :class="{'active':footerActive}"> -->
+  <div class="main login col-lg-4 col-md-6">
+    <div class="content">
+      <img src="img/logo.png" alt>
+      <ul class="nav_group mb-3">
         <li>
-          <router-link to="/">
-            <img src="@/assets/img/icon_footer01.png" alt />
-            <p>菜單</p>
-          </router-link>
+          <router-link to="/login" class="active">登入</router-link>
         </li>
         <li>
-          <router-link to="/cart">
-            <img src="@/assets/img/icon_footer02.png" alt />
-            <p>
-              點菜單
-              <span v-if="footerNumber>0">:{{footerNumber}}項</span>
-            </p>
-          </router-link>
-        </li>
-        <li>
-          <router-link to="/order">
-            <img src="@/assets/img/icon_footer03.png" alt />
-            <p>訂單狀態</p>
-          </router-link>
-        </li>
-        <li class="active">
-          <router-link to="/member">
-            <img src="@/assets/img/icon_footer04.png" alt />
-            <p>會員資訊</p>
-          </router-link>
+          <router-link to="/register">註冊</router-link>
         </li>
       </ul>
-    </footer>
-    <div class="main col-lg-4 col-md-6">
-      <div class="content">
-        <img src="img/logo.png" alt />
-        <ul class="nav_group mb-3">
-          <li>
-            <router-link to="/login" class="active">登入</router-link>
-          </li>
-          <li>
-            <router-link to="/register">註冊</router-link>
-          </li>
-        </ul>
 
-        <div class="form-group">
-          <label class="sr-only" for="phone">電話</label>
-          <span class="iconfont icon-Mobile"></span>
-          <input
-            class="form-control"
-            type="text"
-            id="phone"
-            placeholder="電話"
-            autocomplete="off"
-            v-model="loginInfo.Tel"
-          />
-        </div>
-        <div class="form-group mb-4">
-          <label class="sr-only" for="phone">密碼</label>
-          <span class="iconfont icon-lock"></span>
-          <input
-            class="form-control"
-            type="password"
-            id="password"
-            placeholder="密碼"
-            autocomplete="off"
-            v-model="loginInfo.Password"
-          />
-        </div>
-        <a href="index.html" class="btn btn_default mb-2" @click.prevent="login">登入</a>
-        <br />
-        <small>
-          <a href="#">忘記密碼</a>
-        </small>
+      <div class="form-group">
+        <label class="sr-only" for="phone">電話</label>
+        <span class="iconfont icon-Mobile"></span>
+        <input
+          class="form-control"
+          type="text"
+          id="phone"
+          placeholder="電話"
+          autocomplete="off"
+          v-model="loginInfo.Tel"
+        >
       </div>
+      <div class="form-group mb-4">
+        <label class="sr-only" for="phone">密碼</label>
+        <span class="iconfont icon-lock"></span>
+        <input
+          class="form-control"
+          type="password"
+          id="password"
+          placeholder="密碼"
+          autocomplete="off"
+          v-model="loginInfo.Password"
+        >
+      </div>
+      <a href="index.html" class="btn btn_default mb-2" @click.prevent="login">登入</a>
+      <br>
+      <small>
+        <a href="#">忘記密碼</a>
+      </small>
     </div>
   </div>
-
-  <!-- Mike
-    <h1>Login</h1>
-    <input type="text" placeholder="Tel" v-model="loginInfo.Tel" />
-    <input type="text" placeholder="Password" v-model="loginInfo.Password" />
-    <button @click.prevent="login">登入</button>
-    <button @click.prevent="ReSendSMS" v-if="resendAppear">重新發送認證簡訊</button>
-    <div v-if="vertifyAppear">
-      <input type="text" placeholder="Vertify" v-model="vertifyCode" />
-      <button @click.prevent="vertify(loginInfo.Tel,vertifyCode)">17.Vertify確認驗證碼</button>
-  </div>
-  -->
 </template>
 <script>
 export default {
@@ -115,20 +69,20 @@ export default {
           this.$http.post(url, data, config).then(response => {
             console.log(response);
             if (response.data == "success") {
-              alert(response.data);
+              this.$swal("登入成功", "歡迎一起躺著點！", "success");
               this.$router.push({ name: "Member" });
             } else if (response.data == "此電話號碼尚未進行驗證") {
-              alert(response.data);
+              this.$swal(response.data, "請驗證您的電話號碼！", "warning");
               vm.resendAppear = true;
             } else {
-              alert(response.data);
+              this.$swal("哎呦！登入失敗", response.data, "warning");
             }
           });
         } else {
-          alert("請輸入正確手機格式");
+          this.$swal("哎呀！請輸入正確手機格式", "", "warning");
         }
       } else {
-        alert("手機長度不符");
+        this.$swal("哎呀！手機長度不符", "", "warning");
       }
     },
     ReSendSMS() {
@@ -137,9 +91,11 @@ export default {
       const url = `${process.env.APIPATH}/Accounts/ReSendSMS?Tel=${params}`;
       this.$http.get(url).then(response => {
         console.log(response);
-        alert(response.data);
         if (response.data !== "已寄發3次驗證碼，請您再次確認電話是否正確") {
           vm.vertifyAppear = true;
+          this.$swal(response.data, "", "warning");
+        } else {
+          this.$swal(response.data, "", "info");
         }
       });
     },
@@ -156,9 +112,9 @@ export default {
       this.$http.post(url, data, config).then(response => {
         console.log(response);
         if (response.data !== "驗證失敗，請重新輸入") {
-          alert("驗證成功");
+          this.$swal("驗證成功", "YA", "success");
         } else {
-          alert("驗證失敗，請重新輸入");
+          this.$swal("驗證失敗，請重新輸入", "QQ", "warning");
         }
       });
     },
@@ -176,5 +132,24 @@ export default {
 };
 </script>
 
-<style scope>
+
+<style scope lang="scss">
+.page {
+  background-image: url(../../../assets/img/full_img.png) !important;
+  background-repeat: no-repeat;
+  background-size: cover;
+  align-items: center;
+  .login {
+    max-width: 400px;
+    background: #fff;
+  }
+}
+@media (min-width: 576px) {
+  footer {
+    max-width: 400px;
+  }
+  .page .content img {
+    display: none;
+  }
+}
 </style>
