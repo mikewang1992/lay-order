58edb7c9138384c555bebf6dafc52c9d0b618b4f
sun8chi <sun8chi@gmail.com>
2019-12-16 17:05:59 +0800

    resultOut footer元件 位置調整

--- src/components/Customer/ResultOut.vue
+++ src/components/Customer/ResultOut.vue
@@ -1,50 +1,48 @@
 <template>
-  <div class="page">
-    <div class="main pb-2">
-      <header>
-        <h1>點餐成功</h1>
-      </header>
-      <div class="content">
-        <div class="result out">
-          <img src="@/assets/img/result_ok.png" alt />
-          <div class="number">001</div>
-          <p class="text-center mb-4">
-            謝謝您的訂餐！
-            <br />請於您指定的時間憑
-            <span class="color_default">取餐編號</span>來店取餐
-            <br />若需調整訂單請撥打電話與我們聯繫！
-          </p>
+  <div class="main pb-2">
+    <header>
+      <h1>點餐成功</h1>
+    </header>
+    <div class="content">
+      <div class="result out">
+        <img src="@/assets/img/result_ok.png" alt>
+        <div class="number">001</div>
+        <p class="text-center mb-4">
+          謝謝您的訂餐！
+          <br>請於您指定的時間憑
+          <span class="color_default">取餐編號</span>來店取餐
+          <br>若需調整訂單請撥打電話與我們聯繫！
+        </p>
+      </div>
+      <div class="order_list mb-4">
+        <ul>
+          <li class="item total">
+            <h4>
+              共
+              <b class="color_default">{{OrderSummary[0].totalQty}}</b> 份
+            </h4>
+            <span>{{OrderSummary[0].totalAmout}}</span>
+          </li>
+          <li class="item">
+            <h4>電話</h4>
+            <span>{{OrderSummary[0].tel}}</span>
+          </li>
+          <li class="item">
+            <h4>取餐人</h4>
+            <span>{{OrderSummary[0].name}}</span>
+          </li>
+          <li class="item">
+            <h4>取餐時間</h4>
+            <span>{{getTime(OrderSummary[0].gettime)}}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="d-flex text-center">
+        <div class="col-6">
+          <a href="#" class="btn btn_lg btn_yellow d-block">撥打電話</a>
         </div>
-        <div class="order_list mb-4">
-          <ul>
-            <li class="item total">
-              <h4>
-                共
-                <b class="color_default">{{OrderSummary[0].totalQty}}</b> 份
-              </h4>
-              <span>{{OrderSummary[0].totalAmout}}</span>
-            </li>
-            <li class="item">
-              <h4>電話</h4>
-              <span>{{OrderSummary[0].tel}}</span>
-            </li>
-            <li class="item">
-              <h4>取餐人</h4>
-              <span>{{OrderSummary[0].name}}</span>
-            </li>
-            <li class="item">
-              <h4>取餐時間</h4>
-              <span>{{getTime(OrderSummary[0].gettime)}}</span>
-            </li>
-          </ul>
-        </div>
-        <div class="d-flex text-center">
-          <div class="col-6">
-            <a href="#" class="btn btn_lg btn_yellow d-block">撥打電話</a>
-          </div>
-          <div class="col-6">
-            <router-link class="btn btn_lg btn_default d-block">訂單狀態</router-link>
-          </div>
+        <div class="col-6">
+          <router-link class="btn btn_lg btn_default d-block">訂單狀態</router-link>
         </div>
       </div>
     </div>
