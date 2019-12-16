44f02fa3c9ced491f9657ac80fbeb5a4c6fc514f
sun8chi <sun8chi@gmail.com>
2019-12-16 17:17:16 +0800

    新增 footer 點菜單的數量span

--- src/components/Customer/Footer.vue
+++ src/components/Customer/Footer.vue
@@ -1,30 +1,33 @@
 <template>
   <footer>
     <div class="nav_logo">
-      <img src="@/assets/img/logo_nav.png" alt />
+      <img src="@/assets/img/logo_nav.png" alt>
     </div>
     <ul>
       <li :class="{'active':footerActive == 'pageHome'}">
         <router-link to="/Product">
-          <img src="@/assets/img/icon_footer01.png" alt />
+          <img src="@/assets/img/icon_footer01.png" alt>
           <p>菜單</p>
         </router-link>
       </li>
       <li :class="{'active':footerActive=='pageOrder'}">
         <router-link to="/cart">
-          <img src="@/assets/img/icon_footer02.png" alt />
-          <p>點菜單</p>
+          <img src="@/assets/img/icon_footer02.png" alt>
+          <p>
+            點菜單
+            <!-- <span v-if="footerNumber>0">:{{footerNumber}}項</span> -->
+          </p>
         </router-link>
       </li>
       <li :class="{'active':footerActive=='pageStatus'}">
         <router-link to="/order">
-          <img src="@/assets/img/icon_footer03.png" alt />
+          <img src="@/assets/img/icon_footer03.png" alt>
           <p>訂單狀態</p>
         </router-link>
       </li>
       <li :class="{'active':footerActive=='pageMember'}">
         <router-link to="/member">
-          <img src="@/assets/img/icon_footer04.png" alt />
+          <img src="@/assets/img/icon_footer04.png" alt>
           <p>會員中心</p>
         </router-link>
       </li>
@@ -36,7 +39,7 @@
 export default {
   data() {
     return {
-      footerActive: ''
+      footerActive: ""
     };
   },
   methods: {
