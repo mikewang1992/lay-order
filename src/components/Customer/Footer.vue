316ad11ab9b1b036999c637d27ceb434384922bf
sun8chi <sun8chi@gmail.com>
2019-12-17 12:57:26 +0800

    點菜端 - 修改 footer Active 效果

--- src/components/Customer/Footer.vue
+++ src/components/Customer/Footer.vue
@@ -4,13 +4,13 @@
       <img src="@/assets/img/logo_nav.png" alt>
     </div>
     <ul>
-      <li :class="{'active':footerActive == 'pageHome'}">
+      <li :class="{'active':pageActive=='home'}">
         <router-link to="/Product">
           <img src="@/assets/img/icon_footer01.png" alt>
           <p>菜單</p>
         </router-link>
       </li>
-      <li :class="{'active':footerActive=='pageOrder'}">
+      <li :class="{'active':pageActive=='cart'}">
         <router-link to="/cart">
           <img src="@/assets/img/icon_footer02.png" alt>
           <p>
@@ -19,13 +19,13 @@
           </p>
         </router-link>
       </li>
-      <li :class="{'active':footerActive=='pageStatus'}">
+      <li :class="{'active':pageActive=='order'}">
         <router-link to="/order">
           <img src="@/assets/img/icon_footer03.png" alt>
           <p>訂單狀態</p>
         </router-link>
       </li>
-      <li :class="{'active':footerActive=='pageMember'}">
+      <li :class="{'active':pageActive=='member'}">
         <router-link to="/member">
           <img src="@/assets/img/icon_footer04.png" alt>
           <p>會員中心</p>
@@ -39,16 +39,40 @@
 export default {
   data() {
     return {
-      footerActive: ""
+      pageActive: ""
     };
   },
   methods: {
-    urltest() {
-      console.log();
+    footerActive() {
+      let vm = this;
+      let nowURL = location.href.split("#/")[1];
+      this.pageActive = nowURL;
+      if (nowURL == "") {
+        nowURL = "home";
+        vm.pageActive = nowURL;
+        console.log(nowURL);
+      } else {
+        vm.pageActive = nowURL;
+        console.log(nowURL);
+      }
+    },
+    checkFooterCart() {
+      // console.log('123')
+      // if (JSON.parse(localStorage.getItem("totalcart")) !== null) {
+      //   this.footerNumber = JSON.parse(
+      //     localStorage.getItem("totalcart")
+      //   ).length;
+      // }
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.footerActive()
     }
   },
   created() {
-    this.urltest();
+    this.footerActive();
+    this.checkFooterCart();
   }
 };
 </script>
